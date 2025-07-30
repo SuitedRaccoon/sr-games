98033a420126ae66836ee02ba2ba3afbd527b65c
SuitedRaccoon <gustavosaud@gmail.com>
2025-07-30 17:22:20 -0300

    implementada ideia de funcionamento do loop principal do jogo[B

--- petepel/app.py
+++ petepel/app.py
@@ -10,15 +10,9 @@ class Carta:
         print(f"    ID:     {self.nome}")
 
 #instancias
-pedra   = Carta(1, "Pedra")
-papel   = Carta(2, "Papel")
-tesoura = Carta(3, "Tesoura")
-
-CARTAS = {
-    1: 'PEDRA',
-    2: 'PAPEL',
-    3: 'TESOURA'
-}
+CARTAS = {1: Carta(1, "Pedra"), 
+          2: Carta(2, "Papel"), 
+          3: Carta(3, "Tesoura")}
 
 # tabela de quem ganha de quem: chave vence de valor
 VITORIAS = {
@@ -32,17 +26,20 @@ def compara(c1:Carta, c2:Carta) -> str:
     if VITORIAS.get(c1.id) == c2.id: return f"{c1.nome} vence {c2.nome}... Vitória!"
     else: return f"{c2.nome} vence {c1.nome}... Derrota."
 
-def escolha_player() -> int:
+def escolha_player() -> Carta:
     print("1 - PEDRA | 2 - PAPEL | 3 - TESOURA")
     escolha:int = int(input("Digite sua escolha: "))
-    return escolha
+    return CARTAS[escolha]
 
-def escolha_cpu() -> int:
-    return random.randint(1, 3)
+def escolha_cpu() -> Carta:
+    return CARTAS[random.randint(1, 3)]
+
+def main() -> None:
+    carta1:Carta = escolha_player()
+    carta2:Carta = escolha_cpu()
+    print(f"\nVocê escolheu {carta1.nome}")
+    print(f"A CPU escolheu {carta2.nome}\n\n")
+    print(compara(carta1, carta2))
 
 if __name__ == "__main__":
-    carta1 = papel
-    carta2 = CARTAS.get(escolha_cpu())
-    print(f"A CPU escolheu {carta2}")
-    print(compara(carta1, carta1))
-    print(compara(carta1, carta2))
+    main()
