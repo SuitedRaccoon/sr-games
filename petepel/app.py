cc3c9b2963652917d04362b749f7681f70ff3c5f
SuitedRaccoon <gustavosaud@gmail.com>
2025-07-30 17:06:09 -0300

    testando ideias de comparação rng

--- petepel/app.py
+++ petepel/app.py
@@ -1,33 +1,48 @@
-import random, time
+import random
 
-MAX_INT = 2**31 - 1
+class Carta:
+    def __init__(self, id:int, nome:str) -> None:
+        self.id = id
+        self.nome = nome
 
-def somasegura(a, b):
-    if a > MAX_INT - b:
-        raise OverflowError(f"Soma excede o valor máximo permitido de {MAX_INT}")
-    return a + b
+    def exibe_info(self):
+        print(f"    Carta:  {self.id}")
+        print(f"    ID:     {self.nome}")
 
-n = 10000
-m = 10000
+#instancias
+pedra   = Carta(1, "Pedra")
+papel   = Carta(2, "Papel")
+tesoura = Carta(3, "Tesoura")
 
-amostra = []
-bigsoma = 0
+CARTAS = {
+    1: 'PEDRA',
+    2: 'PAPEL',
+    3: 'TESOURA'
+}
 
-inicio = time.time()
+# tabela de quem ganha de quem: chave vence de valor
+VITORIAS = {
+    1: 3,  # Pedra (1) vence Tesoura (3)
+    2: 1,  # Papel (2) vence Pedra (1)
+    3: 2,  # Tesoura (3) vence Papel (2)
+}
 
-for i in range(m):
-    soma = 0
-    for j in range(n):
-        gerado = random.randint(0, 20)
-        soma = somasegura(soma, gerado)
-    media = soma/n
-    print(f"dado {i} de {m}")
-    bigsoma = somasegura(bigsoma, soma)
-    amostra.append(soma)    
+def compara(c1:Carta, c2:Carta) -> str:
+    if c1.id == c2.id: return "Empate!"
+    if VITORIAS.get(c1.id) == c2.id: return f"{c1.nome} vence {c2.nome}... Vitória!"
+    else: return f"{c2.nome} vence {c1.nome}... Derrota."
 
-bigmedia = bigsoma/(n*m)
-print(f"\nvalor total = {bigsoma}")
-print(f"bigmedia = {bigmedia}")
+def escolha_player() -> int:
+    print("1 - PEDRA | 2 - PAPEL | 3 - TESOURA")
+    escolha:int = int(input("Digite sua escolha: "))
+    return escolha
 
-fim = time.time()
-print(f"Tempo de execução: {fim - inicio:.4f} segundos")
+def escolha_cpu() -> int:
+    return random.randint(1, 3)
+
+if __name__ == "__main__":
+    carta1 = papel
+    carta2 = CARTAS.get(escolha_cpu())
+    print(f"A CPU escolheu {carta2}")
+    print(compara(carta1, carta1))
+    print(compara(carta1, carta2))
